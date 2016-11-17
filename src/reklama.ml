f42f7fe905e73a24cabb82ed704e346730fe6c74
Marek Kubica <marek@xivilization.net>
2016-11-17 22:42:08 +0100

    Create data model

--- src/reklama.ml
+++ src/reklama.ml
@@ -1,4 +1,25 @@
 open Containers
 
+type timestamp = float
+
+type interest = string
+
+type channel = {
+  name: string;
+  categories: interest list
+}
+
+type channel_views = (channel * int CCLock.t)
+
+type ad = {
+  id: int;
+  starting: timestamp;
+  ending: timestamp;
+  views: int CCLock.t;
+  channels: channel_views list
+}
+
+type database = ad list
+
 let () =
   print_endline "Hello World"
