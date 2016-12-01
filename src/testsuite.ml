8497f3c353e977fafb1700ceb34f544386e5d7b8
Marek Kubica <marek@xivilization.net>
2016-12-01 22:33:00 +0100

    Add test to make sure all ads returned have the proper category

--- src/testsuite.ml
+++ src/testsuite.ml
@@ -1,3 +1,5 @@
+open Containers
+
 let ad_id = 23
 
 let channel = Reklama.{
@@ -12,6 +14,14 @@ let ad = QCheck.(
   |> map @@ fun views ->
     (views,
      Reklama.Ad.build ad_id Ptime.min Ptime.max views uri [(channel, views / 2)] ["travel"]))
+
+let ad_maybe_with_interest = QCheck.(
+  (pair pos_int bool) |> map @@ fun (id, include_travel) ->
+    Reklama.Ad.build id Ptime.min Ptime.max max_int uri [] (if include_travel then ["travel"] else []))
+
+let ads_with_interests = QCheck.(list ad_maybe_with_interest)
+
+let db_with_interests = QCheck.(ads_with_interests |> map @@ fun ads -> Reklama.Ad.db_of_ad_list ads)
 
 let proper_amount_of_views = QCheck.(Test.make ~count:1000 ad @@
   fun (views, ad) ->
@@ -43,10 +53,35 @@ let proper_amount_of_channel_views = QCheck.(Test.make ~count:1000 ad @@
     let uris = loop db [] in
     List.length uris = channel_views)
 
+let all_matches_proper_interest = QCheck.(Test.make ~count:1000 db_with_interests @@
+  fun db ->
+    let channel_name = Some Reklama.(channel.name) in
+    let rec loop db categories = function
+      | 0 -> categories
+      | n ->
+        match Reklama.find_matching_ad db channel_name ["travel"] (Ptime_clock.now ()) with
+        | None -> categories
+        | Some ad ->
+            match Reklama.Ad.view channel_name ad db with
+            | (None, _) -> categories
+            | (_, db) -> loop db ((Reklama.Ad.categories channel_name ad)::categories) (n-1)
+    in
+    let categories = loop db [] 100 in
+    let has_no_travel_category categories =
+      categories
+      |> Reklama.Categories.to_list
+      |> List.find_pred (fun x -> x = "travel")
+      |> CCOpt.is_none
+    in
+    match categories |> List.find_pred has_no_travel_category with
+    | None -> true
+    | Some _ -> false)
+
 let main () =
   QCheck_runner.run_tests_main [
     proper_amount_of_views;
     proper_amount_of_channel_views;
+    all_matches_proper_interest;
   ]
 
 let () =
